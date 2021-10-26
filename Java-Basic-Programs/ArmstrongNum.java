53434000b4a7e46d86d3762ea24598a8f02c8d0d
Alen Abraham <53680255+abrahamalen@users.noreply.github.com>
2021-10-27 00:16:14 +0530

    armstrong number solution added

---
+++ Java-Basic-Programs/ArmstrongNum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+// To find Armstrong Number between two given number.
+
+public class ArmstrongNum {
+    public static void main(String[] args) {
+        int num1, num2;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        num1 = sc.nextInt();
+        System.out.println("Enter the second number: ");
+        num2 = sc.nextInt();
+
+        for (int i = num1; i < num2; i++) {
+            int check, rem, sum = 0;
+            check = i;
+            while (check != 0) {
+                rem = check % 10;
+                sum = sum + (rem * rem * rem);
+                check = check / 10;
+            }
+            if (sum == i) {
+                System.out.println("" + i + " is an Armstrong number.");
+            }
+        }
+        sc.close();
+    }
+}
