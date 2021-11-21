bab354b3846100b59a1e8dac1f4a418f05dc3c2e
Alen Abraham <53680255+abrahamalen@users.noreply.github.com>
2021-11-21 12:48:52 +0530

    program added

---
+++ conditionals-loops/Intermediate-programs/EvenDays.java
@@ -0,0 +1,13 @@
+
+// Kunal is allowed to go out with his friends only on the even days of a given month. Write a program to count the number of days he can go out in the month of August.
+
+public class EvenDays {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i <= 30; i++) {
+            if (i % 2 == 0)
+                count++;
+        }
+        System.out.println("No: of even days is: " + count);
+    }
+}
