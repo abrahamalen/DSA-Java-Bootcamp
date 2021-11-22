70b8af9afcdf464d539c23de7be2b1f4183fce3e
Alen Abraham <53680255+abrahamalen@users.noreply.github.com>
2021-11-22 10:34:57 +0530

    refactored

--- conditionals-loops/Intermediate-programs/NumberSum.java
+++ conditionals-loops/Intermediate-programs/NumberSum.java
@@ -1,3 +1,10 @@
+
+/*
+Write a program to print the sum of negative numbers, 
+sum of positive even numbers and the sum of positive odd numbers from a list of numbers (N) entered by the user. 
+The list terminates when the user enters a zero.
+*/
+
 import java.util.Scanner;
 
 public class NumberSum {
