bbfd6c93ea3717a7a6db9017958599fd4dcd788a
Alen Abraham <53680255+abrahamalen@users.noreply.github.com>
2021-11-22 10:35:42 +0530

    refactored

--- conditionals-loops/Intermediate-programs/EvenDays.java
+++ conditionals-loops/Intermediate-programs/EvenDays.java
@@ -1,5 +1,8 @@
 
-// Kunal is allowed to go out with his friends only on the even days of a given month. Write a program to count the number of days he can go out in the month of August.
+/*
+Kunal is allowed to go out with his friends only on the even days of a given month. 
+Write a program to count the number of days he can go out in the month of August.
+*/
 
 public class EvenDays {
     public static void main(String[] args) {
