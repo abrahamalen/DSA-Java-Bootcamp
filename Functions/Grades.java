c43941648c18af9cab718933a31668d7b11ba5a2
Alen Abraham <53680255+abrahamalen@users.noreply.github.com>
2021-11-23 11:50:39 +0530

    program added

--- Functions/Grades.java
+++ Functions/Grades.java
@@ -9,3 +9,33 @@ Marks        Grade
 41-50          DD 
 <=40          Fail 
 */
+
+import java.util.Scanner;
+
+public class Grades {
+    public static void main(String[] args) {
+        System.out.println("Enter marks: ");
+        Scanner in = new Scanner(System.in);
+        int marks = in.nextInt();
+        studentGrade(marks);
+        in.close();
+    }
+
+    static void studentGrade(int marks) {
+        if (marks > 90 && marks <= 100)
+            System.out.println("Grade: AA");
+        else if (marks <= 90 && marks > 80)
+            System.out.println("Grade: AB");
+        else if (marks <= 80 && marks > 70)
+            System.out.println("Grade: AB");
+        else if (marks <= 70 && marks > 60)
+            System.out.println("Grade: AB");
+        else if (marks <= 60 && marks > 50)
+            System.out.println("Grade: AB");
+        else if (marks <= 50 && marks > 40)
+            System.out.println("Grade: AB");
+        else
+            System.out.println("Fail");
+    }
+
+}
