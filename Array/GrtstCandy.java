1ebdc403829c70b1438c084fad1e9cf52200a127
Alen Abraham <53680255+abrahamalen@users.noreply.github.com>
2021-11-25 17:22:42 +0530

    solution added

--- Array/GrtstCandy.java
+++ Array/GrtstCandy.java
@@ -34,3 +34,22 @@ Constraints:
     1 <= candies[i] <= 100
     1 <= extraCandies <= 50
 */
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int n = candies.length;
+        ArrayList<Boolean> result = new ArrayList<Boolean>();
+        int maxCandies = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++) {
+            if(candies[i] > maxCandies)
+                maxCandies = candies[i];
+        }
+        for(int i = 0; i < n; i++) {
+            if(candies[i] + extraCandies >= maxCandies)
+                result.add(true);
+            else
+                result.add(false);
+        }
+        return result;
+    }
+}
