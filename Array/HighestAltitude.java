85db0cd5b42b219160ceb52983ef4c70746b8a09
Alen Abraham <53680255+abrahamalen@users.noreply.github.com>
2021-12-01 23:46:25 +0530

    solution added

--- Array/HighestAltitude.java
+++ Array/HighestAltitude.java
@@ -3,7 +3,7 @@ Leetcode - Easy
 Find the Highest Altitude
 
 There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
-You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). 
+You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i and i + 1 for all (0 <= i < n). 
 Return the highest altitude of a point.
 
 Example 1:
@@ -21,3 +21,15 @@ Constraints:
     1 <= n <= 100
     -100 <= gain[i] <= 100
 */
+
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int high = 0, low = 0;
+        for(int i = 0; i < gain.length; i++) {
+            low += gain[i];
+            if(low > high)
+                high = low;
+        }
+        return high;
+    }
+}
