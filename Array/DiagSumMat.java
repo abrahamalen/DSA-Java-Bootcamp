a0b2334c1e926d6951712bf372e208d5b75dfbdf
Alen Abraham <53680255+abrahamalen@users.noreply.github.com>
2021-12-27 23:12:52 +0530

    solution added

--- Array/DiagSumMat.java
+++ Array/DiagSumMat.java
@@ -28,3 +28,26 @@ Constraints:
     1 <= n <= 100
     1 <= mat[i][j] <= 100
 */
+
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int n = mat.length;
+        int sum = 0;
+        
+        //Summing up diagonal 1
+        for(int i=0;i<n;i++){
+            sum += mat[i][i];
+        }
+        
+        //Summing up diagonal 2
+        for(int i=0;i<n;i++){
+            sum += mat[i][n-i-1];
+        }
+        
+        //If element repeating (in case n odd) subtracting that
+        if(n%2==1){
+            sum -= mat[n/2][n/2];
+        }
+        return sum;
+    }
+}
