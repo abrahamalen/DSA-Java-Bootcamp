6201348ca8b8f2cf60299f53accc7c119e12256f
Alen Abraham <53680255+abrahamalen@users.noreply.github.com>
2022-01-03 16:39:23 +0530

    solution added

--- Array/EvenDgtNum.java
+++ Array/EvenDgtNum.java
@@ -25,3 +25,19 @@ Constraints:
     1 <= nums.length <= 500
     1 <= nums[i] <= 105
 */
+
+class Solution {
+    public int findNumbers(int[] nums) {
+        int result = 0;
+        for(int n:nums) {
+            int count = 0;
+            while(n != 0) {
+                n = n / 10;
+                count++;
+            }
+            if(count % 2 == 0)
+                result++;
+        }
+        return result;
+    }
+}
