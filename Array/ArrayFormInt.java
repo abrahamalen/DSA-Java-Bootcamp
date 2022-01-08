9ca570b5a5871a3664dd8828b7aab9fba3780e0c
Alen Abraham <53680255+abrahamalen@users.noreply.github.com>
2022-01-09 00:06:33 +0530

    solution added

--- Array/ArrayFormInt.java
+++ Array/ArrayFormInt.java
@@ -26,3 +26,24 @@ Constraints:
     num does not contain any leading zeros except for the zero itself.
     1 <= k <= 104
 */
+
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+         ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=num.length-1; i>=0 || k>0; i--){
+            
+            if(i>=0){
+                list.add((num[i]+k)%10);
+                k=(num[i]+k)/10;
+            }
+            else{
+                list.add(k%10);
+                k/=10;
+            }
+        }
+        
+        Collections.reverse(list);
+      return list;
+    }
+}
