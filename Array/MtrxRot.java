e9017af22eaed2a9354e15a56e03d7f887e451df
Alen Abraham <53680255+abrahamalen@users.noreply.github.com>
2022-01-13 20:21:13 +0530

    solution added

--- Array/MtrxRot.java
+++ Array/MtrxRot.java
@@ -24,3 +24,36 @@ Constraints:
     1 <= n <= 10
     mat[i][j] and target[i][j] are either 0 or 1.
 */
+
+class Solution {
+    
+    public boolean findRotation(int[][] mat, int[][] target) {
+        for (int i = 0; i < 4; i++) {
+            int rotate[][] = rotate(mat);
+            if (isEquals(rotate, target))
+                return true;
+            mat = rotate;
+        }
+        return false;
+    }
+
+    public int[][] rotate(int mat[][]) {
+        int rows = mat.length, cols = mat[0].length;
+        int result[][] = new int[rows][cols];
+        for (int row = 0, i = 0; row < rows; ++row, ++i) {
+            for (int col = cols - 1, j = 0; col >= 0; --col, ++j) {
+                result[i][j] = mat[col][row];
+            }
+        }
+        return result;
+    }
+
+    public boolean isEquals(int mat1[][], int mat2[][]) {
+        for (int row = 0; row < mat1.length; ++row) {
+            for (int col = 0; col < mat1.length; ++col) {
+                if (mat1[row][col] != mat2[row][col]) return false;
+            }
+        }
+        return true;
+    }
+}
